d48cc32c789c0140bee5ac3bb1a16b96847d2173
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-05-20 13:44:05 +0200

    acabado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,39 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import java.awt.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hola JavaFX"); //Mensaje en la misma ventana
+
+        Button button = new Button("Cambiar mensaje"); //Crea un botón con ese mensaje
+
+        //Acción al pulsar el botón
+            //Cambia el mensaje inicial
+        button.setOnAction(event -> label.setText("Adios JavaFX"));
+
+        //Crea una caja vertical de 10pixel
+        VBox vbox = new VBox(10);
+        //Llena la caja con el mensaje y el botón
+        vbox.getChildren().addAll(label,button);
+
+        //Crea una "escena" o ventana de 400x200px con la caja anterior dentro
+        Scene scene = new Scene(vbox, 400, 200);
+        //Titulo de la "escena" o ventana
+        primaryStage.setTitle("Clic en botón cambia mensaje inicial, Ejercicio 3");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
